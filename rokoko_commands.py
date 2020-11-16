4a238de37aa1af26c41995d83fb36a94e5596ec5
Andreas Block <job@andreasblock.de>
2020-11-16 15:10:17 +0100

    Commented the code

--- rokoko_commands.py
+++ rokoko_commands.py
@@ -1,9 +1,17 @@
+# In Cinema 4D CommandData plugins represent commands, usually represented in the UI as
+# menu entries or icons in command palettes.
+#
+# In Rokoko Studio Live there currently is only one command, which opens and owns the
+# Rokoko Studio Live Manager dialog.
 import c4d
 from rokoko_ids import *
 from rokoko_utils import *
 from rokoko_dialog_manager import *
 
+# The dialog needs to be globally accessible, so it can be properly shut down in PluginMessage()
 g_dlgManager = None
+
+# To be called during shutdown
 def CommandsDestroyGlobals():
     global g_dlgManager
     if g_dlgManager is not None:
@@ -11,10 +19,13 @@ def CommandsDestroyGlobals():
             c4d.gui.MessageDialog('SEVERE ISSUE: FAILED TO CLOSE DIALOG')
         g_dlgManager = None
 
+
 class CommandDataRokokoManager(c4d.plugins.CommandData):
     _dlg = None
 
+    # Called by C4D, when the user executes the command
     def Execute(self, doc):
+        # If there is already a dialog, reuse it, otherwise create one
         global g_dlgManager
         if self._dlg is None:
             self._dlg = g_dlgManager
@@ -23,18 +34,32 @@ class CommandDataRokokoManager(c4d.plugins.CommandData):
             g_dlgManager = self._dlg
         if self._dlg is None:
             return False
+
+        # Force the dialog to gather tags from scene
         c4d.SpecialEventAdd(PLUGIN_ID_COREMESSAGE_MANAGER, CM_SUBID_MANAGER_UPDATE_TAGS)
+
+        # Open the dialog
         return self._dlg.Open(dlgtype=c4d.DLG_TYPE_ASYNC, pluginid=PLUGIN_ID_COMMAND_MANAGER, defaulth=0, defaultw=0)
 
+
+    # Called by C4D to restore the dialog (e.g. after the user switched the UI layout)
     def RestoreLayout(self, sec_ref):
+        # If there is no dialog, yet, create one
         global g_dlgManager
         if self._dlg is None:
             self._dlg = DialogRokokoManager()
             g_dlgManager = self._dlg
+
+        # Restore the dialog
         return self._dlg.Restore(pluginid=PLUGIN_ID_COMMAND_MANAGER, secret=sec_ref)
 
+
+    # Called by C4D to detemine the state of this command, if it's enable or active
     def GetState(self, doc):
+        # The command is always enabled
         state = c4d.CMD_ENABLED
+
+        # If the manager dialog is open, reflect this in command's icon highlighting
         if self._dlg is not None and self._dlg.IsOpen():
             state |= c4d.CMD_VALUE
         return state
--- rokoko_prefs.py
+++ rokoko_prefs.py
@@ -1,8 +1,24 @@
+# PreferenceData plugins allow to hook a plugin into Cinema 4D's preferences and have their
+# own preferences page.
+#
+# The Rokoko Studio Live plugin has almost no parameters, that need to be handled via a PreferenceData.
+# Actually only one: The option to enable and disable the Rokoko Studio Live plugin.
+#
+# This PreferenceData is also the only plugin, Rokoko Studio Live registers,
+# even if the user decided to disable the plugin. For obvious reasons,
+# it's needed for the user to be able to reenable the plugin.
 import c4d
 from rokoko_ids import *
 from rokoko_utils import *
 from rokoko_description_utils import *
 
+# It seems not to be possible to register a PreferenceData without a description resource,
+# even if one plans to create the content of the preferences page dynamically (as in this plugin).
+# So there's a more or less empty description resource only providing a main group, where all
+# dynamically created parameters will reside in.
+# During development the group ID symbol sometimes went missing. Most likely this shouldn't be an
+# issue for a user. Nevertheless we only try to use this constant and define it on our own,
+# if it's not available.
 try:
     # TODO Andreas: Check with Maxon, how to reliably access resource symbols...
     ROKOKOPREFERENCES_MAIN_GROUP = c4d.ROKOKOPREFERENCES_MAIN_GROUP
@@ -15,24 +31,38 @@ class PreferenceDataRokoko(c4d.plugins.PreferenceData):
         def InitPrefValue(self, description, id, dtype, bcWorldPrefs):
             self.InitPreferenceValue(id, True, description, c4d.DescID(c4d.DescLevel(id, dtype, 0)), bcWorldPrefs)
 
+
+        # Called by C4D to initialize the preference values.
         def Init(self, node, description=None):
             bcWorldPrefs = GetWorldPrefs()
             self.InitPrefValue(description, ID_PREF_PLUGIN_ENABLED, c4d.DTYPE_BOOL, bcWorldPrefs)
             return True
 
+
+        # Called by C4D to set a preference parameter value
         def GetDDescription(self, node, description, flags):
+            # Load the Description resource (C4D caches these internally)
             if not description.LoadDescription('rokokopreferences'):
                 print('{0} ERROR: Failed to load Description'.format(PLUGIN_NAME_COMMAND_MANAGER))
                 return False
+
+            # If default values are requested, reinitialize
             if flags & c4d.DESCFLAGS_DESC_NEEDDEFAULTVALUE:
                 self.Init(node, description)
+
+            # For optimization purposes C4D doesn't always relayout the entire Description.
+            # Based on singleId C4D may request to just relayout a single description parameter.
             singleId = description.GetSingleDescID()
+
+            # Create "Rokoko Studio Live Plugin Enabled" parameter with a checkbox widget
             if not GetDDescriptionCreateBool(node, description, singleId, ID_PREF_PLUGIN_ENABLED, \
                                              'Enable Rokoko Studio Live Plugin (change needs C4D restart to take effect)', \
                                              ROKOKOPREFERENCES_MAIN_GROUP, anim=False, valDefault=True):
                 return False
             return True, flags | c4d.DESCFLAGS_DESC_LOADED
 
+
+        # Called by C4D to set a preference parameter value
         def SetDParameter(self, node, id, data, flags):
             bc = GetWorldPrefs()
             paramID = id[0].id
@@ -40,6 +70,8 @@ class PreferenceDataRokoko(c4d.plugins.PreferenceData):
                 bc.SetBool(paramID, data)
             return True, flags | c4d.DESCFLAGS_SET_PARAM_SET
 
+
+        # Called by C4D to read a preference parameter value
         def GetDParameter(self, node, id, flags):
             bc = GetWorldPrefs()
             paramID = id[0].id
